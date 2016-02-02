80f52efce4c2d3e076a93b4721c4ca0a71f1445b
Matheus Negrão <matheusmoraes.mts@hotmail.com>
2016-02-02 15:13:32 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# CNPJ Generator
+
+CNPJ Dynamic value for [Paw][paw]
+
+### Installation
+```sh
+$ cd ~/Library/Containers/com.luckymarmot.Paw/Data/Library/Application Support/com.luckymarmot.Paw/Extensions
+```
+
+```sh
+$ git clone http://github.com/matheusmoraes/com.matheusmoraes.cnpj.git
+```
+   [paw]: <https://luckymarmot.com/paw>
+
+
---
+++ cnpj.js
@@ -0,0 +1,46 @@
+function random(n) {
+    return Math.round(Math.random()*n);
+}
+
+function digit(dividend, divider) {
+	return Math.round(dividend - (Math.floor(dividend / divider) * divider));
+}
+
+var CNPJ = function() {
+	this.evaluate = function() {
+		var n = 9,
+			n1  = random(n),
+			n2  = random(n),
+			n3  = random(n),
+			n4  = random(n),
+			n5  = random(n),
+			n6  = random(n),
+			n7  = random(n),
+			n8  = random(n),
+			n9  = 0,
+			n10 = 0,
+			n11 = 0,
+			n12 = 1
+		var d1 = n12*2+n11*3+n10*4+n9*5+n8*6+n7*7+n6*8+n5*9+n4*2+n3*3+n2*4+n1*5;
+		d1 = 11 - digit(d1, 11);
+		if (d1 >= 10) d1 = 0;
+		var d2 = d1*2+n12*3+n11*4+n10*5+n9*6+n8*7+n7*8+n6*9+n5*2+n4*3+n3*4+n2*5+n1*6;
+		d2 = 11 - digit(d2,11) ;
+		if (d2 >= 10) d2 = 0;
+		return ''+n1+n2+'.'+n3+n4+n5+'.'+n6+n7+n8+'/'+n9+n10+n11+n12+'-'+d1+d2;
+	}	 
+}
+
+CNPJ.identifier = "com.matheusmoraes.cnpj";
+CNPJ.title = "CNPJ Randomizer";
+registerDynamicValueClass(CNPJ);
+
+
+
+
+
+
+
+
+
+
